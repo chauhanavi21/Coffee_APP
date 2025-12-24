f93b51bca64ce0422d6ad986a34e2db4a1e33b9c
avi212003 <chauhanavi212003@gmail.com>
2025-12-24 13:23:39 -0500

    the attributes for item in the list of categories

---
+++ app/src/main/java/com/example/coffeeapp/adapters/ItemListCategoryAdapter.kt
@@ -0,0 +1,49 @@
+package com.example.coffeeapp.adapters
+
+import android.content.Context
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.uilover.project262.activities.DetailActivity
+import com.uilover.project262.databinding.ViewholderPopularBinding
+import com.uilover.project262.domain.ItemsModel
+import kotlin.io.root
+import kotlin.jvm.java
+
+class ItemListCategoryAdapter(val items: MutableList<ItemsModel>) :
+    RecyclerView.Adapter<ItemListCategoryAdapter.Viewholder>() {
+    lateinit var context: Context
+
+    class Viewholder(val binding: ViewholderPopularBinding) :
+        RecyclerView.ViewHolder(binding.root)
+
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): ItemListCategoryAdapter.Viewholder {
+        context = parent.context
+        val binding = ViewholderPopularBinding.inflate(LayoutInflater.from(context), parent, false)
+        return Viewholder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ItemListCategoryAdapter.Viewholder, position: Int) {
+        holder.binding.titleTxt.text = items[position].title
+        holder.binding.priceTxt.text = "$" + items[position].price.toString()
+        holder.binding.subtitleTxt.text = items[position].extra
+
+        Glide.with(context)
+            .load(items[position].picUrl[0])
+            .into(holder.binding.pic)
+
+        holder.itemView.setOnClickListener {
+        val intent= Intent(context, DetailActivity::class.java)
+            intent.putExtra("object",items[position])
+            context.startActivity(intent)
+        }
+    }
+
+    override fun getItemCount(): Int = items.size
+}
